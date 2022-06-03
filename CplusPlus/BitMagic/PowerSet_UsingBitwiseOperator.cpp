112d4dad94cb4ec49efe7d5c9d71c1c547509b12
rj80099 <rohitjain471@yahoo.com>
2022-06-03 20:13:55 +0530

    Add comment in the last commited program

--- CplusPlus/BitMagic/PowerSet_UsingBitwiseOperator.cpp
+++ CplusPlus/BitMagic/PowerSet_UsingBitwiseOperator.cpp
@@ -1,23 +1,27 @@
+//import all header 
 #include <stdio.h>
 #include <string>
 #include <cmath>
 #include <iostream>
+
+//namespace for better code readability
 using namespace std;
 void powerSet(string str)
 {
+    //get the string length =3 for "abc"
     int n=str.length();
+    //get the 2^n power value to iterate fom 0 to 2^n -1 
     int powerSize=pow(2,n);
-    printf("%d",powerSize);
+
+    // loop through powersize which is 8 for string abc
     for (int counter=0;counter<powerSize;counter++)
     {
+        //loop through each bit position
         for(int j=0;j<n;j++)
         {
-        
-        //printf("%d",(counter &(1<<j)));
-
+        // check the set bit position and print the char
         if((counter &(1<<j))!=0)
         {
-            //printf("%c",str[j]);
             cout<<str[j];
         }
        
